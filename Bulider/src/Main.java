23817e5a29f02d5222062bca59947386d24b2077
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-29 09:29:29 +0100

    Static manufacturing method

--- Bulider/src/Main.java
+++ Bulider/src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        //BUILDER
         House house = new House.HouseBuilder()
                 .setAdress("ul. Kursowa 7")
                 .setDoorsNumber(7)
+                .setFloorsNumber(7)
                 .setWindowsNumber(17)
                 .build();
+
+        //STATIC MANUFACTURING METHOD
+        //from, of, valueOf, instanceOf
+        boolean isTrue = true;
+        Boolean.valueOf(isTrue);
 
     }
 }
