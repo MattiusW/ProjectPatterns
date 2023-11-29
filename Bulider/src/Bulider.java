a2fd47ce16d125109a90b89546c2001af9119fa3
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-29 08:31:39 +0100

    Class builder

---
+++ Bulider/src/Bulider.java
@@ -0,0 +1,17 @@
+public class Bulider {
+
+    String adress;
+    Integer floorsNumber;
+    Integer doorsNumber;
+    Integer windowsNumber;
+
+    Bulider(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber){
+        this.adress = adress;
+        this.floorsNumber = floorsNumber;
+        this.doorsNumber = doorsNumber;
+        this.windowsNumber = windowsNumber;
+    }
+
+
+
+}
